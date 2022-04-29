fc5133898debbaf50fed6a9d230930770c1dd2de
FrozenGarvMonkey <garvnair@gmail.com>
2022-04-29 14:01:34 +0800

    Lab 3

---
+++ Lab 3/Ex1.py
@@ -0,0 +1,9 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as pt
+
+img = cv2.imread("D:\Programming\CSC3104 Computer Vision\Lab 3\sudoku.png", 0)
+retVal, threshImg = cv2.threshold(img, 110, 1, cv2.THRESH_BINARY)
+pt.figure()
+pt.imshow(threshImg, cmap="gray")
+pt.show()
