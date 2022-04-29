a9d8574b371b62969d06f1f6f487fa53d11f3d67
FrozenGarvMonkey <garvnair@gmail.com>
2022-04-29 14:09:51 +0800

    Lab 3 Ex 1 and 2

--- Lab 3/Ex1.py
+++ Lab 3/Ex1.py
@@ -4,6 +4,9 @@ from matplotlib import pyplot as pt
 
 img = cv2.imread("D:\Programming\CSC3104 Computer Vision\Lab 3\sudoku.png", 0)
 retVal, threshImg = cv2.threshold(img, 110, 1, cv2.THRESH_BINARY)
+
+threshImg = cv2.adaptiveThreshold(img,1,cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY,3,0)
+
 pt.figure()
 pt.imshow(threshImg, cmap="gray")
 pt.show()
