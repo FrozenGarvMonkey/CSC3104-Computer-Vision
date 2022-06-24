857ea7d2a91449b62833a0d966a4f35238b31c5c
FrozenGarvMonkey <garvnair@gmail.com>
2022-06-24 15:21:13 +0800

    Lab 3

--- Lab 3/Ex1.py
+++ Lab 3/Ex1.py
@@ -2,6 +2,34 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as pt
 
+def RegionGrowing(img,seeds,thresh,p):
+    height, weight = img.shape
+    seedMark = np.zeros(img.shape)
+    seedList = []
+    for seed in seeds:
+    seedList.append(seed)
+    label = 1
+    connects = selectConnects(p)
+    
+    while(len(seedList)>0):
+    currentPoint = seedList.pop(0)
+
+    seedMark[currentPoint.x,currentPoint.y] = label
+    for i in range(8):
+        tmpX = currentPoint.x + connects[i].x
+        tmpY = currentPoint.y + connects[i].y
+        
+        if tmpX < 0 or tmpY < 0 or tmpX >= height or tmpY >= weight:
+            continue
+        
+        grayDiff = getGrayDiff(img,currentPoint,Point(tmpX,tmpY))
+        
+        if grayDiff < thresh and seedMark[tmpX,tmpY] == 0:
+            seedMark[tmpX,tmpY] = label
+            seedList.append(Point(tmpX,tmpY))
+            
+    return seedMark
+
 img = cv2.imread("D:\Programming\CSC3104 Computer Vision\Lab 3\sudoku.png", 0)
 retVal, threshImg = cv2.threshold(img, 110, 1, cv2.THRESH_BINARY)
 
